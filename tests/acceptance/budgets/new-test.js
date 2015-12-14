7989aaa9420c6be6f6df2853bd5812087e75ce9c
Ryan Tablada <ryan.tablada@gmail.com>
2015-12-14 11:50:11 -0600

    Setup Acceptance for user can create budget

--- tests/acceptance/budgets/new-test.js
+++ tests/acceptance/budgets/new-test.js
@@ -25,3 +25,23 @@ test('User can see form for new budget', function(assert) {
     assert.ok(true);
   });
 });
+
+test('User can create a new budget', function(assert) {
+  visit('/new');
+  fillIn('.budget-input__name', 'Video Game');
+  fillIn('.budget-input__start-date', '12/15/15');
+  fillIn('.budget-input__remaining', '+$1,000,000');
+  click('.budget-submit-btn');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+    let items = find('.budget-list-item');
+    let firstItem = items.first();
+
+    assert.equal(items.length, 1, 'There should be the new budget in the list');
+
+    assert.equal(firstItem.find('.budget-list-item__name').text(), 'Video Game');
+    assert.equal(firstItem.find('.budget-list-item__start-date').text(), '12/15/15');
+    assert.equal(firstItem.find('.budget-list-item__remaining').text(), '+$1,000,000');
+  });
+});
