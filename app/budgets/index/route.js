9038d20ee14e7bafb780fa14d9af8cd0c42a2817
Ryan Tablada <ryan.tablada@gmail.com>
2015-12-14 11:20:44 -0600

    Use each loop to show list of budgets

--- app/budgets/index/route.js
+++ app/budgets/index/route.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return [
+      {name: 'Groceries', startDate: '12/1/15', remaining: '+$100'},
+      {name: 'Entertainment', startDate: '12/1/15', remaining: '+$20'},
+      {name: 'Christmas Gifts', startDate: '12/1/15', remaining: '+$120'},
+    ];
+  },
 });
