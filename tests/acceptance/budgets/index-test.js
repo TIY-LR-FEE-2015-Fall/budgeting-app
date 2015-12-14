114bb6b5910f4a49e53899875b01a623cdfd9090
Ryan Tablada <ryan.tablada@gmail.com>
2015-12-14 11:15:52 -0600

    User can see static list of budgets

--- tests/acceptance/budgets/index-test.js
+++ tests/acceptance/budgets/index-test.js
@@ -13,3 +13,18 @@ test('visiting / shows all budgets', function(assert) {
     assert.equal(currentRouteName(), 'budgets.index');
   });
 });
+
+test('User can see a list of budgets', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    let items = findWithAssert('.budget-list-item');
+    let firstItem = items.first();
+
+    assert.equal(items.length, 3, 'There should be three budgets in the list');
+
+    assert.equal(firstItem.find('.budget-list-item__name').text(), 'Groceries');
+    assert.equal(firstItem.find('.budget-list-item__start-date').text(), '12/1/15');
+    assert.equal(firstItem.find('.budget-list-item__remaining').text(), '+$100');
+  });
+});
